fedb0a39c5d09292deddb7ce48f064b18dbdc9e8
baconsoccer <30930432+baconsoccer@users.noreply.github.com>
2019-02-02 00:20:38 +0100

    Add files via upload

--- animatie.js
+++ animatie.js
@@ -1,9 +1,14 @@
 const knop = document.getElementById('knop');
-let vlak = document.querySelector('.vlakje');
+let vlak = document.querySelector('.vlak');
+let vlak2 = document.querySelector('.vlak2');
+let vlak3 = document.querySelector('.vlak3');
+
 
 
 const schuif = () => {
-    vlak.classList.toggle('vlakje--schuif-uit');
+    vlak.classList.toggle('vlak--schuif-uit');
+    vlak2.classList.toggle('vlak2--schuif-uit');
+    vlak3.classList.toggle('vlak3--schuif-uit');
 }
 
 knop.addEventListener('click', schuif);
