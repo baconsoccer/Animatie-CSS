814479884346948acfee808ab783117316f1ec70
baconsoccer <30930432+baconsoccer@users.noreply.github.com>
2019-02-25 13:58:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.getElementById('button');
+let vlak = document.querySelector('.vlak');
+let vlak2 = document.querySelector('.vlak2');
+let vlak3 = document.querySelector('.vlak3');
+const schuif = () => {
+    vlak.classList.toggle('vlak--schuif-uit');
+    vlak2.classList.toggle('vlak2--schuif-uit');
+    vlak3.classList.toggle('vlak3--schuif-uit');
+}
+
+button.addEventListener('click', schuif);
+
